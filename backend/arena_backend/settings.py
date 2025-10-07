e8e25d8c8b75515aa547ec8d3f7d9c174987f32c
Ishvinder Sethi <ishvindersethi22@gmail.com>
2025-10-07 14:47:41 +0530

    Update settings.py

--- backend/arena_backend/settings.py
+++ backend/arena_backend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = "django-insecure-@r7r$^v&pkqi*%plz(obg#2yt0hie(^-*3t1@j28v+o0fly@-#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:3000', 'localhost', '127.0.0.1', 'https://backend.dev.arena.ai4bharat.org']
+ALLOWED_HOSTS = ['http://127.0.0.1:3000', 'localhost', '127.0.0.1', 'https://backend.dev.arena.ai4bharat.org', 'backend.dev.arena.ai4bharat.org']
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
